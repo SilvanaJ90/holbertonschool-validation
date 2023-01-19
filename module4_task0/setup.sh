c1de5e36c3cf3e3a2d7f50f4ebc81d4eaf4b7bdb
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-01-18 22:14:47 -0500

    update1

--- module4_task0/setup.sh
+++
@@ -1,5 +0,0 @@
-apt-get update
-apt-get install -y hugo make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
-dpkg -i hugo_extended_0.109.0_linux-amd64.deb
-rm -f hugo_extended_0.109.0_linux-amd64.deb
